6d244f9eea44322d28fa224403f897d034887a7b
Gabriel Viana <gaa.vianaa@gmail.com>
2025-04-29 12:00:21 -0300

    class ConectaBD

---
+++ src/Database/ConexaoBD.php
@@ -0,0 +1,38 @@
+<?php 
+namespace ExemploCrud;
+
+use Exception;
+use PDO;
+use Throwable;
+
+abstract class ConexaoBD 
+{
+    private static PDO $conexao;
+    private static string $servidor = "localhost";
+    private static string $usuario = "root";
+    private static string $senha = "";
+    private static string $banco = "vendas";
+
+    public static function getConexao():PDO 
+    {
+        if(!isset( self::$conexao )){
+            try {
+
+                self::$conexao = new PDO(
+                    "mysql:host=".self::$servidor.";dbname=".self::$banco.";charset=utf8",
+                    self::$usuario, self::$senha
+                );
+            
+                self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch ( Throwable $erro) {
+             
+                // Registrar a exceção em um arquivo de log/txeto interno
+
+                // Lançar uma mensagem de erro generica sem detalhes do banco/sistema
+                throw new Exception( "Erro ao conectar");
+            }
+        }
+        return self::$conexao;
+    }
+}
+
